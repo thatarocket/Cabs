2c16ceb4e3cff8c1e1fe6f5222ae150772030a6a
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-10-20 21:06:26 -0300

    Histórias login

---
+++ features/step_definitions/login_usuarios.step.rb
@@ -0,0 +1,11 @@
+Dado('que estou na página de login do usuário') do
+    visit '/login'
+end
+  
+Quando('clico em entrar') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Então('devo conseguir ter acesso ao sistema') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
