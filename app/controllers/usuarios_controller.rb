3714523d36dedcbd33d5d270f630c4f957cae0b4
Guilherme Sena <Guilherme.Sena@ibm.com>
2022-10-21 17:08:35 -0300

    edit user implemented

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -18,6 +18,21 @@ class UsuariosController < ApplicationController
         @usuario = Usuario.find(params[:id])
     end
 
+    def edit
+        @usuario = Usuario.find(params[:id])
+    end
+    
+    def update
+        @usuario = Usuario.find(params[:id])
+    
+        if @usuario.update(usuario_params)
+            redirect_to @usuario
+        else
+            render :edit, status: :unprocessable_entity, content_type: "text/html"
+            headers["Content-Type"] = "text/html"
+        end
+    end
+
     private
     def usuario_params
         params.require(:usuario).permit(:username,:email,:senha)
