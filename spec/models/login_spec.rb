ebf987c171db4597a89fea98a3e2c1c12eb3b725
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-10-26 16:31:15 -0300

    fix

--- spec/models/login_spec.rb
+++ spec/models/login_spec.rb
@@ -1,17 +1,15 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Login, type: :model do
   it 'invalido sem nome de usuario' do
-    usuario = Usuario.new
-    usuario.email = "thaisTeste@gmail.com"
+    usuario = Login.new
     usuario.senha = "teste1234"
     expect(usuario).not_to be_valid
   end
 
   it 'invalido sem senha' do
-    usuario = Usuario.new
+    usuario = Login.new
     usuario.username = "thais-teste"
-    usuario.email = "thaisTeste@gmail.com"
     expect(usuario).not_to be_valid
   end
 end
