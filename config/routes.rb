830dd3a136caa9009ba8e2b4225201273b81bd2c
gustavos1lva <gustavosilva1@usp.br>
2022-11-05 17:21:43 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
   # root to: "application#index"
-  root to: "logins#new"
+  root to: "welcome#homepage"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
