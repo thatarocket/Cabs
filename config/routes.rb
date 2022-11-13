9a040b27b3ce281ec5e8f011b084bfc6c1a8c8f4
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-11-13 16:40:51 -0300

    rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :item_trocas
+  
   # root to: "application#index"
   root to: "logins#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   resources :usuarios
   resource :login, only: [:new, :create]
+  resources :item_trocas
   
   # Login e cadastro 
   get "/application", to: "application#index"
@@ -15,5 +15,8 @@ Rails.application.routes.draw do
 
   get "login", to: "logins#new"
   post "login", to: "logins#create"
+
+  # Pesquisa
+  get "/item_trocas/search", to: "item_trocas#search"
 
 end
