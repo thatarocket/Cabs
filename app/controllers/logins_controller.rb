665684d771ba88dea4b9dff7e7d3cf3d81682ea8
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-11-30 19:06:21 -0300

    trying to fix code smell

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -5,22 +5,28 @@ class LoginsController < ApplicationController
     
     def create
         @login = Login.new(usuario_params)
-
-        if @login.valid?
-            @usuario = Usuario.find_by(username: usuario_params[:username])
-            if @usuario.present? && @usuario.authenticate(usuario_params[:password])
-                session[:user_id] = @usuario.id
-                redirect_to :application, notice: "Login efetuado com sucesso!"
-            else
-                flash[:alert] = "Username ou senha inválidos!"
-                render :new, status: :unprocessable_entity, content_type: "text/html"
-                headers["Content-Type"] = "text/html"
-            end
+        if @login.valid?            
+            valid
         else
-            render :new, status: :unprocessable_entity, content_type: "text/html"
-            headers["Content-Type"] = "text/html"
+            not_valid
         end
     end  
+
+    def valid
+        @usuario = Usuario.find_by(username: usuario_params[:username])
+        if @usuario.present? && @usuario.authenticate(usuario_params[:password])
+            session[:user_id] = @usuario.id
+            redirect_to :application, notice: "Login efetuado com sucesso!"
+        else
+            not_valid
+        end
+    end
+
+    def not_valid
+        flash[:alert] = "Username ou senha inválidos!"
+        render :new, status: :unprocessable_entity, content_type: "text/html"
+        headers["Content-Type"] = "text/html"
+    end
     
     def destroy
         session[:user_id] = nil
