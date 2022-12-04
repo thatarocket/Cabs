ed4623a985919d5ea78ab37da3c4545ec5d5b9df
fichimura <fabichimura@gmail.com>
2022-12-04 14:37:06 -0300

    teste de edição passando

--- features/step_definitions/edicao_usuarios_step.rb
+++ features/step_definitions/edicao_usuarios_step.rb
@@ -1,8 +1,8 @@
 Dado('que estou na página de edição de usuário') do
     visit '/usuarios/new'
-    fill_in "Username", :with => 'a'
-    fill_in "Email", :with => 'a@gmail.com'
-    fill_in "Password", :with => 'ab'
+    fill_in "Username", :with => 'thais-teste'
+    fill_in "Email", :with => 'thaisTeste@gmail.com'
+    fill_in "Password", :with => 'teste1234'
     click_on 'Salvar novo usuário'
     visit '/usuarios/1/edit'
   end
