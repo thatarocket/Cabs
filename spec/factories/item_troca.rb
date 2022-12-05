c391cb509eba35405edd6174533b3d2d9987c75a
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-12-04 21:20:25 -0300

    itens factor

--- spec/factories/item_troca.rb
+++ spec/factories/item_troca.rb
@@ -5,4 +5,18 @@ FactoryBot.define do
         categoria { 'Objetos' }
         condicao { 'Usado' }
     end
+
+    factory :item_troca2, class: ItemTroca do
+        nome { 'Blusa Harry Potter' }
+        descricao { 'Harry Potter e o Cálice de fogo' }
+        categoria { 'Vestiário' }
+        condicao { 'Usado' }
+    end
+
+    factory :item_troca3, class: ItemTroca do
+        nome { 'Sapato Harry Potter' }
+        descricao { 'Harry Potter e o Cálice de fogo' }
+        categoria { 'Sapatos' }
+        condicao { 'Usado' }
+    end
   end
