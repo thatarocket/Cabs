baf0e8d44e52ace68f98fa50d8bf07b5890670ce
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-12-06 19:21:42 -0300

    fix tests

--- features/step_definitions/exibir_itens_step.rb
+++ features/step_definitions/exibir_itens_step.rb
@@ -1,4 +1,5 @@
 Dado('não exista um item na categoria') do
-    find(:xpath,'/html/body/div[2]/section[2]/p[2]').text == 'Não há itens nesta categoria'
+    find(:xpath,'//*[@id="Acessórios"]/div/p').text == 'Não há itens nesta categoria'
+    
 end
 
