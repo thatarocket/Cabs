7db8e3313ffd36089b34ebfd5819fb6f81b5d632
thatarocket <thais.souza.rodrigues20722@gmail.com>
2022-12-05 23:44:40 -0300

    refatorando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,52 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       @usuario = Usuario.find_by(id: session[:user_id])
     end
+    categories
+    item_troca
+  end
 
-    # For the see item
-    if ItemTroca.all.count > 0
-      @categories = ["Roupas", "Acessórios", "Sapatos", "Maquiagens","Objetos", "Outros"] 
-      @items = ItemTroca.all #Itens que eu tenho atualmente
-    end
+  def categories
+    @categories = ["Roupas", "Acessórios", "Sapatos", "Maquiagens","Objetos", "Outros"] 
+  end
+
+  def all_items
+    @items = ItemTroca.all #Itens que eu tenho atualmente
+  end
+
+  def item_troca
+    roupas
+    acessórios
+    sapatos
+    maquiagens
+    objetos
+    outros
+  end
+
+  def roupas
+    @Roupas = ItemTroca.where(categoria: "Roupas")
+  end
+
+  def acessórios
+    @Acessórios = ItemTroca.where(categoria: "Acessórios")
+  end
+
+  def sapatos
+    @Sapatos = ItemTroca.where(categoria: "Sapatos")
+  end
+
+  def maquiagens
+    @Maquiagens = ItemTroca.where(categoria: "Maquiagens")
+  end
+
+  def objetos
+    @Objetos = ItemTroca.where(categoria: "Objetos")
+  end
+
+  def outros
+    @Outros = ItemTroca.where(categoria: "Outros")
+  end
+
+  def show
+    
   end
 end
